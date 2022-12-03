ae683f569669c9a1078a0b852ac8794a173555dc
elijaholmos <eli.olmos9@gmail.com>
2022-12-03 13:37:05 -0700

    style: comment app file

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const reader = require('rpi-mfrc522-nodejs');
 const Terminal = require('./Terminal');
 
-const READER_PINS = [[2, 22]];
+const READER_PINS = [[2, 22]]; // GPIO pins
 
 const handleRead = function (data) {
 	const [card_id] = data ?? [];
-	if (!card_id) return;
+	if (!card_id) return; // ignore empty reads
 
 	// call the api or something
+
+	// remove loading spinner, show result table
 	Terminal.hideSpinner();
 	Terminal.sendResultTable(card_id);
 	Terminal.showSpinner();
 };
 
+// initialize the terminal and show loading spinner
 Terminal.init().showSpinner();
 
+// listen to new card reads
 reader.onRfidChange(READER_PINS, handleRead);
